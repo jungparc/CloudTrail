8ea246d1ed42f4458e13592702f7825cba346560
byounggeol-cho <byounggeol.cho@nhn.com>
2023-09-11 09:45:34 +0900

    NGSC 에서의 특별 처리 추가  - console guide 는 따로 만들도록 수정  - Governance%20&%20Audit 링크는 없어지도록 수정

--- replace.sh
+++ replace.sh
@@ -1,10 +1,11 @@
 sed -e "s/event-list/event-list-gov/g" ko/api-guide.md > ko/api-guide-gov.md
+sed -e "s/\/Governance%20&%20Audit//g" ko/api-guide.md > ko/api-guide-ngsc.md
 sed -e "s/event-list/event-list-ngsc/g" ko/api-guide.md > ko/api-guide-ngsc.md
+
 
 git add ko/api-guide-gov.md ko/api-guide-ngsc.md
 
 sed -e "s/s3-api-guide/s3-api-guide-gov/g" ko/console-guide.md > ko/console-guide-gov.md
-sed -e "s/s3-api-guide/s3-api-guide-ngsc/g" ko/console-guide.md > ko/console-guide-ngsc.md
 
 git add ko/console-guide-gov.md ko/console-guide-ngsc.md
 
